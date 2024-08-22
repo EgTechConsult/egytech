ec1a7012b0962663a56e30a713589c92ba797b51
O-Osman1618 <57672809+O-Osman1618@users.noreply.github.com>
2024-08-22 23:45:30 +0200

    more theme options (#3)
    Co-authored-by: Osman Osman <osman.osman@MacBook-Pro-von-Osman.local>

--- styles/appBarStyle.tsx
+++ styles/appBarStyle.tsx
@@ -1,9 +1,15 @@
 import { SxProps, Theme } from "@mui/material";
 
-export const appBarStyles: SxProps<Theme> = {
+export const appBarIconStyle: SxProps<Theme> = (theme) => ({
   position: "fixed",
   top: 0,
   left: 0,
   width: "100%",
-  zIndex: (theme) => theme.zIndex.drawer + 1, // Ensure it sits above other content
+  zIndex: theme.zIndex.drawer + 1, // Ensure it sits above other content
+  color: theme.custom?.primary?.background_text_1,
+  background: theme.custom?.primary?.background_1,
+});
+
+export const appBarContainerStyle: SxProps<Theme> = {
+  background: (theme) => theme.custom?.primary?.background_1,
 };
